0013da4faf5db009b54954f5270f7b86c4dc3921
varvara bobrova <varia.bobrova@gmail.com>
2022-01-28 20:56:48 +0200

    init: create morse decoder

--- src/index.js
+++ src/index.js
@@ -35,11 +35,46 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let decoded = '';
+    function morseToBinary(str) { // transform morse to binary
+        let binary = '';
+        for (let i = 0; i < str.length; ++i) {            
+            if (str[i] === '.') {
+                binary += '10';    
+            } else if (str[i] === '-') {
+                binary += '11';
+            }
+        }
+        if (binary.length < 10) {
+           binary = '0'.repeat(10 - binary.length) + binary;
+        }
+        return binary;
+    }
+
+    function renameKey(obj, old_key, new_key) { // rename object keys
+        obj[new_key] = obj[old_key];
+    }
+
+    for (let key in MORSE_TABLE) { // rewrite MORSE_TABLE
+        renameKey(MORSE_TABLE, key, morseToBinary(key));
+    }
+    
+    for (let i = 0; i < expr.length - 9; i += 10) {
+        let char = expr.slice(i, i + 10);
+        for (let key in MORSE_TABLE) { 
+            if (char === key) {               
+                decoded += MORSE_TABLE[key];
+            }
+        }
+    }
+    return decoded;
 }
+
+
 
 module.exports = {
     decode
